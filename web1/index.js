9fc221a48eadd0e4a5c18839485c2a91556716d2
jozenua <jozenua@gmail.com>
2018-02-04 17:08:56 -0600

    First code Assignment

---
+++ web1/index.js
@@ -0,0 +1,17 @@
+(function() {
+
+    var canvas = document.getElementById(canvas);
+    var content = document.getContext("2d");
+
+
+function myFace() {
+    var x = 20;
+    var y = 60;
+
+    ccontent.moveTo(x, y);
+    content.arc(x, y, Math.PI * 8, Math.PI / 6, Math.PI * 7 / 6, anticlockwise);
+    content.fillStyle = "#01579B";
+    content.fill(circle);
+
+}
+}) ();
